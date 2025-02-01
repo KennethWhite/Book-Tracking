6ff316e1d9db36fca4754f7b98144cd86692498e
Kenny White <kennival@hotmail.com>
2025-01-31 17:49:26 -0800

    Sample test working
    Signed-off-by: Kenny White <kennival@hotmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
+import path from 'path';
 import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@test-utils': path.resolve(__dirname, 'test-utils'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.mjs'],
+  },
 });
