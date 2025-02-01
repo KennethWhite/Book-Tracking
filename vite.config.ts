d42177b48b89a6cc033e6c26a3ec79038ef20df3
Kenny White <kennival@hotmail.com>
2025-01-31 18:19:57 -0800

    Add mantine template, gitignore (#1)
    * Mantine template, minus yarn
    * Update gitignore
    * Update GHA cache
    * GHA add branch filters
    * Run prettier, normalize line endings
    * Run prettier, normalize line endings
    * Update tsconfig globbing for test utils
    * Sample test working
    
    ---------
    
    Signed-off-by: Kenny White <kennival@hotmail.com>

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,18 @@
-import {defineConfig} from 'vite'
-import react from '@vitejs/plugin-react'
+import path from 'path';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vitest/config';
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react()],
-})
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@test-utils': path.resolve(__dirname, 'test-utils'),
+    },
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['./vitest.setup.mjs'],
+  },
+});
