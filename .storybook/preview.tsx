8c1cfdf33beb36e867491cc3f6c0b37d96a3e7dc
Kenny White <kennival@hotmail.com>
2025-01-31 16:56:53 -0800

    Run prettier, normalize line endings
    Signed-off-by: Kenny White <kennival@hotmail.com>

--- .storybook/preview.tsx
+++ .storybook/preview.tsx
@@ -1,3 +1,6 @@
+
+
+
 import '@mantine/core/styles.css';
 
 import React, { useEffect } from 'react';
@@ -5,6 +8,7 @@ import { addons } from '@storybook/preview-api';
 import { DARK_MODE_EVENT_NAME } from 'storybook-dark-mode';
 import { MantineProvider, useMantineColorScheme } from '@mantine/core';
 import { theme } from '../src/theme';
+
 
 const channel = addons.getChannel();
 
