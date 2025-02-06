472394f8db4b3121d62362c383c0bf656c62cfc0
Kenny White <kennival@hotmail.com>
2025-02-05 20:53:39 -0800

    Add a Layout page and routing
    Signed-off-by: Kenny White <kennival@hotmail.com>

---
+++ BookTracking.Web/src/pages/Layout.tsx
@@ -0,0 +1,30 @@
+import { Outlet } from 'react-router-dom';
+import { AppShell, Burger } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+
+export function Layout() {
+  const [opened, { toggle }] = useDisclosure();
+
+  return (
+    <AppShell
+      header={{
+        height: 60,
+      }}
+      navbar={{
+        width: 300,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened, desktop: !opened },
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Burger opened={opened} onClick={toggle} visibleFrom="sm" size="lg" />
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
+      <AppShell.Main>
+        <Outlet />
+      </AppShell.Main>
+    </AppShell>
+  );
+}
